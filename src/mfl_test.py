0fc6fcabe840f478a8d952095f9b25f387efd886
WOLFinBULLcity <kaherndo+github@gmail.com>
2018-04-26 21:08:47 -0400

    backing up tinker files

--- src/mfl_test.py
+++ src/mfl_test.py
@@ -3,6 +3,7 @@ from model.auction_result import Auction_Result
 
 import pprint
 import json
+import dpath.util
 
 mflTest = Api(2018)
 
@@ -11,7 +12,6 @@ mflTest = Api(2018)
 # print(mflTest.nfl_schedule())
 
 # pprint.pprint(mflTest)
-#
 # pprint.pprint(mflTest.league(26496))
 
 results = mflTest.auction_results(26496)
@@ -23,30 +23,75 @@ results = mflTest.auction_results(26496)
 # pprint.pprint(results)
 # pprint.pprint(parsed_json)
 
-completed_auctions = results['auctionResults']
+# completed_auctions = results['auctionResults']
+#
+# auction = completed_auctions['auctionUnit']
 
-auction = completed_auctions['auctionUnit']
+# something = [x['auction'] for x in results['auctionResults']['auctionUnit'].values()]
+# pprint.pprint(results['auctionResults']['auctionUnit'][0]['unit'])
 
-ct = 1
-ct_b = 1
-ct_c = 1
-for a in auction:
-  # print('IDK #' + str(ct))
-  # print(type(a))
-  # pprint.pprint(a)
-  ct_b = 1
-  for b in a.values():
-    print('IDK #' + str(ct) + '.' + str(ct_b))
-    # print(type(b))
-    # pprint.pprint(b)
-    ct_b += 1
-    if isinstance(b, list):
-      for c in b:
-        auct = Auction_Result(c)
-        pprint.pprint(c)
-        print(auct.winningBid)
-        ct_c += 1
-  ct += 1
+leagues = dpath.util.values(results, "auctionResults/auctionUnit/*/unit")
+drafts = dpath.util.values(results, "auctionResults/auctionUnit/*/auction")
+
+# print(results)
+
+for idx in range(len(leagues)):
+  # print(leagues[idx])
+  # print(drafts[idx])
+  # print("Players from Auction #" + str(idx))
+  for auction in drafts[idx]:
+    if isinstance(auction, dict):
+      player_id = auction['player']
+      player_info = mflTest.players(
+          players=[player_id],
+          details=True
+      )['players']['player']
+
+      player_name = player_info['name']
+      player_pos = player_info['position']
+
+      # print(player_info)
+
+      # print(auction)
+      #
+      # print(
+      #     "Player ID: " + player_id + ", " +
+      #     "Player Name: " + player_name + ", " +
+      #     "Player Position: " + player_pos
+      # )
+
+      # franchise_id = auction['franchise']
+      # franchise_info = mflTest.league(
+      #     league_id=26496,
+      #     franchise_id=franchise_id
+      # )
+      #
+      # print(franchise_info)
+
+# player_object = mflTest.players(players=[13128])
+# pprint.pprint(player_object)
+
+# ct = 1
+# ct_b = 1
+# ct_c = 1
+# for draft in auction:
+#   print('IDK #' + str(ct))
+#   ct_b = 1
+#   pprint.pprint(draft.keys())
+#
+#   conference = draft['unit']
+#   completed_auctions = draft['auction']
+#
+  # for b in conference_draft.values():
+  #   # print(type(b))
+  #   ct_b += 1
+  #   if isinstance(b, list):
+  #     for c in b:
+  #       auct = Auction_Result(c)
+  #       # pprint.pprint(c)
+  #       # print(auct.winningBid)
+  #       ct_c += 1
+  # ct += 1
 
 # pprint.pprint(auc_res)
 
